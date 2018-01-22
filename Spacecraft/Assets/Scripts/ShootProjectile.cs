192d5e61b64f36af670739dbd6c812420109b952
Gilbert Vrancken <vrancken.gilbert@gmail.com>
2018-01-22 21:41:58 +0100

    added some notes

--- Spacecraft/Assets/Scripts/ShootProjectile.cs
+++ Spacecraft/Assets/Scripts/ShootProjectile.cs
@@ -21,34 +21,32 @@ public class ShootProjectile : MonoBehaviour {
 	void Update ()
     {
         //Put the ProjectileScript into variable.
+        // Notes(gb): Don't do this every tick: the same script is needed each tick, 
+        // so scope this higher than the Update function (= as class variable)
         ProjectileScript psc = projectile.GetComponent<ProjectileScript>();
 
-        //When clicking the mouse.
         if (Input.GetMouseButtonDown(0))
         {
-            //An raycast will be shot from the camera.
             RaycastHit hit;
             Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, raycastLength);
-            //Checking if the hit object has a rigidbody.(Better then collider, doesn't depend on (no) parents)
+            //Checking if the hit object has a rigidbody.(Better than collider, doesn't depend on (no) parents)
             if (hit.rigidbody != null)
             {
                 //Put the Spacecraft script into variable for easier use.
+                // Notes(gb): more importantly: you grab the object only once,
+                // and from then on use a locally scoped variable, which saves a lot of CPU power.
                 Spacecraft sc = hit.rigidbody.GetComponent<Spacecraft>();
-                //Checking if the hit object has the Spacecraft script on it (instead of tags).
+                
                 if (sc != null)
                 {
                     //Checking if there is no cooldown going on.(Don't use coroutines for cooldowns, keep it simple and all in a row).
-                    if(Time.time > sc.timeTilWeCanShoot)
+                    if (Time.time > sc.timeTilWeCanShoot)
                     {
-                        //Setting the cooldown until we can shoot again.
+  
                         sc.timeTilWeCanShoot = Time.time + sc.shootCooldown;
-                        //Instantiating the projectile and adding it to the list.
                         projectileList.Add((GameObject)Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation));
-                        //Determining which projectile in the list this is, -1 because the list starts at 0.
                         currentProjectile = projectileList.Count - 1;
-                        //Giving the projectile its velocity.
                         projectileList[currentProjectile].GetComponent<Rigidbody>().velocity = transform.forward * psc.projectileSpeed;
-                        //Setting the time until explosion for each individual projectile.
                         projectileList[currentProjectile].GetComponent<ProjectileScript>().timeWhenExplodes = Time.time + projectileList[currentProjectile].GetComponent<ProjectileScript>().secondsToLive;
                         Debug.Log("Spawned & set time");
                     }
