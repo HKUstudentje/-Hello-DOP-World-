4a37e9ecfc8371caf0d41fcd62563053885d65e1
HKUstudentje <tbenjaminh@gmail.com>
2018-01-25 22:30:11 +0100

    Recommit of the ShootProjectile script

--- Spacecraft/Assets/Scripts/ShootProjectile.cs
+++ Spacecraft/Assets/Scripts/ShootProjectile.cs
@@ -8,9 +8,11 @@ public class ShootProjectile : MonoBehaviour {
     public Transform projectileSpawner;
     public Transform cameraTransform;
     public float raycastLength;
+
     public int currentProjectile;
 
-    public List<GameObject> projectileList = new List<GameObject>();
+    public List<ProjectileScript> projectileList = new List<ProjectileScript>();
+    public List<ProjectileScript> disabledProjectileList = new List<ProjectileScript>();
     public List<GameObject> explosionList = new List<GameObject>();
 
     void Start ()
@@ -42,28 +44,37 @@ public class ShootProjectile : MonoBehaviour {
                     //Checking if there is no cooldown going on.(Don't use coroutines for cooldowns, keep it simple and all in a row).
                     if (Time.time > sc.timeTilWeCanShoot)
                     {
-  
+
+                        //Setting the cooldown until we can shoot again.
                         sc.timeTilWeCanShoot = Time.time + sc.shootCooldown;
-                        projectileList.Add((GameObject)Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation));
+                        //Instantiating the projectile as a new gameObject variable, and adding it's script to the list.
+                        GameObject newProjectile = Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation);
+                        projectileList.Add(newProjectile.GetComponent<ProjectileScript>());
+                        //Giving the projectile its velocity.
+                        newProjectile.GetComponent<Rigidbody>().velocity = transform.forward * psc.projectileSpeed;
+                        //Determining which projectile in the list this is, -1 because the list starts at 0.
                         currentProjectile = projectileList.Count - 1;
-                        projectileList[currentProjectile].GetComponent<Rigidbody>().velocity = transform.forward * psc.projectileSpeed;
-                        projectileList[currentProjectile].GetComponent<ProjectileScript>().timeWhenExplodes = Time.time + projectileList[currentProjectile].GetComponent<ProjectileScript>().secondsToLive;
-                        Debug.Log("Spawned & set time");
+                        //Setting the time until explosion for each individual projectileScript using the currentProjectile(the projectile number).
+                        projectileList[currentProjectile].timeWhenExplodes = Time.time + projectileList[currentProjectile].secondsToLive;
                     }
                 }
             }
         }
-        //For every projectile in the projectileList...
-        foreach(GameObject projectile in projectileList)
+        //Set int i to the amount of projectiles at the beginning of the loop; for as long as i is bigger then 0 (as long as there are projectiles) the loop plays; i - 1 at the end of the loop;
+        for (int i = projectileList.Count; i > 0; i--)
         {
-            //...we check if it is time to explode...
-            if(Time.time > projectile.GetComponent<ProjectileScript>().timeWhenExplodes)
+            if (Time.time > projectileList[i - 1].timeWhenExplodes)
             {
-                Debug.Log("Boom");
                 //and we instantiate an explosion at the projectile location.
-                Instantiate(explosion, projectile.transform.position, Quaternion.identity);
-                Destroy(projectile.gameObject);
+                Instantiate(explosion, projectileList[i - 1].transform.position, Quaternion.identity);
+                disabledProjectileList.Add(projectileList[i - 1]);
+                projectileList.RemoveAt(i - 1);
             }
+        }
+
+        foreach (ProjectileScript projectileScript in disabledProjectileList)
+        {
+            //Disable projectile? Or do it in the for loop! Might be better. 
         }
     }
 }
