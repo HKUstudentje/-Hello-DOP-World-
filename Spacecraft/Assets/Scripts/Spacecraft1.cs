41c0d81af5611580c3d12d9395fc186c75ea242e
HKUstudentje <tbenjaminh@gmail.com>
2018-02-02 10:42:28 +0100

    PowerUp added

---
+++ Spacecraft/Assets/Scripts/Spacecraft1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spacecraft : MonoBehaviour {
+    public float shootCooldown;
+    public float timeTilWeCanShoot;
+    public int spaceCraftSpeed;
+    public int spaceCraftRotateSpeed;
+}
